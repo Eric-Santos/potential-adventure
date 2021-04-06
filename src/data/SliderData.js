bd06f32c500098a33133cca5c4da36897896adae
eric santos <eric355402@yahoo.com>
2021-04-05 20:19:06 -0600

    basic navbar, repeated images on hero section, menu bar not working properly on mobile, needs more images medium sized

---
+++ src/data/SliderData.js
@@ -0,0 +1,45 @@
+// import ImageOne from "../images/fish.jpg";
+import ImageOne from "../images/sardines.jpg";
+
+export const SliderData = [
+  {
+    title: "Luxury Villa in Bali",
+    price: "$4,280,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageOne,
+    alt: "House",
+  },
+  {
+    title: "Luxury Villa in Bali",
+    price: "$4,280,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageOne,
+    alt: "House",
+  },
+  {
+    title: "Luxury Villa in Bali",
+    price: "$4,280,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageOne,
+    alt: "House",
+  },
+  {
+    title: "Luxury Villa in Bali",
+    price: "$4,280,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageOne,
+    alt: "House",
+  },
+  {
+    title: "Luxury Villa in Bali",
+    price: "$4,280,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageOne,
+    alt: "House",
+  },
+];
