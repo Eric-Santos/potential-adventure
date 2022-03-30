f30a6403dc7d33e1634aaeb3f6dab3ef62527a46
Eric Santos <eric355402@yahoo.com>
2022-03-30 13:03:53 -0600

    updated images, needs optimizing and functional links

--- src/data/InfoData.js
+++ src/data/InfoData.js
@@ -1,26 +1,26 @@
-import ImageOne from "../images/fireVertical.jpg";
-import ImageTwo from "../images/newYork.jpg";
+import ImageOne from '../images/yogaVertical.jpg';
+import ImageTwo from '../images/newYork.jpg';
 
 export const InfoData = {
-  headings: "Explore our homes",
-  paragraphOne:
-    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat aspernatur consequuntur, praesentium est maxime officia ullam quibusdam nisi officiis quam fugit error dicta odit earum animi. Dignissimos iusto cumque aspernatur.",
-  paragraphTwo:
-    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis reprehenderit saepe in unde ratione modi aperiam provident libero voluptas! Molestiae?",
-  buttonLabel: "View Projects",
-  image: ImageOne,
-  reverse: false,
-  delay: 100,
+	headings: 'Grow With Us',
+	paragraphOne:
+		'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat aspernatur consequuntur, praesentium est maxime officia ullam quibusdam nisi officiis quam fugit error dicta odit earum animi. Dignissimos iusto cumque aspernatur.',
+	paragraphTwo:
+		'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis reprehenderit saepe in unde ratione modi aperiam provident libero voluptas! Molestiae?',
+	buttonLabel: 'View Availability',
+	image: ImageOne,
+	reverse: false,
+	delay: 100,
 };
 
 export const InfoDataTwo = {
-  headings: "Modern Designs",
-  paragraphOne:
-    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat aspernatur consequuntur, praesentium est maxime officia ullam quibusdam nisi officiis quam fugit error dicta odit earum animi. Dignissimos iusto cumque aspernatur.",
-  paragraphTwo:
-    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis reprehenderit saepe in unde ratione modi aperiam provident libero voluptas! Molestiae?",
-  buttonLabel: "View Projects",
-  image: ImageTwo,
-  reverse: true,
-  delay: 300,
+	headings: 'Modern Designs',
+	paragraphOne:
+		'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat aspernatur consequuntur, praesentium est maxime officia ullam quibusdam nisi officiis quam fugit error dicta odit earum animi. Dignissimos iusto cumque aspernatur.',
+	paragraphTwo:
+		'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis reprehenderit saepe in unde ratione modi aperiam provident libero voluptas! Molestiae?',
+	buttonLabel: 'View Projects',
+	image: ImageTwo,
+	reverse: true,
+	delay: 300,
 };
--- src/data/SliderData.js
+++ src/data/SliderData.js
@@ -1,58 +1,59 @@
 // import ImageOne from "../images/fish.jpg";
-import ImageOne from "../images/city.jpg";
-import ImageTwo from "../images/sardines.jpg";
-import ImageThree from "../images/skeleton.jpg";
-import ImageFour from "../images/bridge.jpg";
-import ImageFive from "../images/citytwo.jpg";
-import ImageSix from "../images/building.jpg";
+import ImageOne from '../images/yogaCity.jpg';
+import ImageTwo from '../images/yogaSit.jpg';
+// import ImageThree from '../images/skeleton.jpg';
+// import ImageFour from '../images/bridge.jpg';
+// import ImageFive from '../images/citytwo.jpg';
+// import ImageSix from '../images/building.jpg';
 
 export const SliderData = [
-  {
-    title: "Coco Loko",
-    price: "$4,280,000",
-    path: "/projects",
-    label: "View Projects",
-    image: ImageOne,
-    alt: "House",
-  },
-  {
-    title: "Project Two",
-    price: "$4,280,000",
-    path: "/projects",
-    label: "View Projects",
-    image: ImageTwo,
-    alt: "House",
-  },
-  {
-    title: "Project Three",
-    price: "$4,280,000",
-    path: "/projects",
-    label: "View Projects",
-    image: ImageThree,
-    alt: "House",
-  },
-  {
-    title: "Project Four",
-    price: "$4,280,000",
-    path: "/projects",
-    label: "View Projects",
-    image: ImageFour,
-    alt: "House",
-  },
-  {
-    title: "Project Five",
-    price: "$4,280,000",
-    path: "/projects",
-    label: "View Projects",
-    image: ImageFive,
-    alt: "House",
-  },
-  {
-    title: "Coco Loko",
-    price: "$4,280,000",
-    path: "/projects",
-    label: "View Projects",
-    image: ImageSix,
-    alt: "House",
-  },
+	{
+		// link to calendly
+		title: 'Yoga Studio',
+		price: '',
+		path: '/projects',
+		label: 'View Availability',
+		image: ImageOne,
+		alt: 'Yoga Rooftop',
+	},
+	{
+		title: 'Yoga Studio',
+		price: '',
+		path: '/projects',
+		label: 'View Availability',
+		image: ImageTwo,
+		alt: 'Yoga Pose',
+	},
+	// {
+	// 	title: 'Project Three',
+	// 	price: '$4,280,000',
+	// 	path: '/projects',
+	// 	label: 'View Projects',
+	// 	image: ImageThree,
+	// 	alt: 'House',
+	// },
+	// {
+	// 	title: 'Project Four',
+	// 	price: '$4,280,000',
+	// 	path: '/projects',
+	// 	label: 'View Projects',
+	// 	image: ImageFour,
+	// 	alt: 'House',
+	// },
+	// {
+	// 	title: 'Project Five',
+	// 	price: '$4,280,000',
+	// 	path: '/projects',
+	// 	label: 'View Projects',
+	// 	image: ImageFive,
+	// 	alt: 'House',
+	// },
+	// {
+	// 	title: 'Coco Loko',
+	// 	price: '$4,280,000',
+	// 	path: '/projects',
+	// 	label: 'View Projects',
+	// 	image: ImageSix,
+	// 	alt: 'House',
+	// },
 ];
